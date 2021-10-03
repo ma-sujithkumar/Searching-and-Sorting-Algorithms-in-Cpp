c9a3323fe92b9a1d6dafe8c92844b8c4b4c1ae54
Sujithkumar M A <masujithkumar@gmail.com>
2021-10-03 15:52:47 +0530

    Update QuickSort.cpp

--- QuickSort.cpp
+++ QuickSort.cpp
@@ -1,66 +1,50 @@
 #include<iostream>
 using namespace std;
-/* Quick Sort
-1.Partitioning function
-2. Quick Sort
-3. Swapping function
-*/
 void swap(int* x,int* y)
 {
-    int temp=*x;
+    int temp;
+    temp=*x;
     *x=*y;
     *y=temp;
 }
-
-int partition(int array[],int first,int last)
+int partition(int arr[],int low,int high)
 {
-    int pivot=array[first];
-    int i=first;
-    int j=last;
-    
+    int pivot=arr[low];
+    int i=low;
+    int j=high;
     do
     {
-        do{i++;}while(array[i]<=pivot);
-        do{j--;}while(array[i]>pivot);
-        
-        if(i<j)
-        swap(&array[i],&array[j]);
-        
+        do
+        {
+            i++;
+        }while(arr[i]<=pivot);
+        do
+        {
+            j--;
+        }while(arr[j]>pivot);
+        if (i<j)
+        swap(&arr[i],&arr[j]);
     }while(i<j);
-    
-    //swapping the pivot element with current j
-    swap(&array[first],&array[j]);
+    swap(&arr[j],&arr[low]);
     return j;
 }
-
-void QuickSort(int array[],int first,int last)
+void QuickSort(int arr[],int low,int high)
 {
-    int j;
-    if(first<last)
+    if(low<high)
     {
-        j=partition(array,first,last);
-        QuickSort(array,first,j);
-        QuickSort(array,j+1,last);
+    int p=partition(arr,low,high);
+    QuickSort(arr,low,p);
+    QuickSort(arr,p+1,high);
     }
 }
-
-
-
 int main()
 {
-    int array[]={10,2,34,21,45,13};
-    int size=6;
-    
-    //printing the elements:
-    for(int i=0;i<size;i++)
+    int arr[]={51,45,63,71,43,23,90};
+    int n=sizeof(arr)/sizeof(arr[0]);
+    QuickSort(arr,0,n-1);
+    int i;
+    for(i=0;i<n;i++)
     {
-        cout<<array[i]<<" ";
-    }
-    QuickSort(array,0,size-1);
-    
-    //printing the elements:
-    for(int i=0;i<size;i++)
-    {
-        cout<<array[i]<<" ";
+        cout<<arr[i]<<" ";
     }
 }
